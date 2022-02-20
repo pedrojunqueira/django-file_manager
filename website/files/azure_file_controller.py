aa1a4841c36c47e227e4e04ce8c628fedb16ecd7
Pedro Junqueira <pedrocj@gmail.com>
2022-02-20 14:53:22 +1030

    final demo working version

--- website/files/azure_file_controller.py
+++ website/files/azure_file_controller.py
@@ -1,13 +1,16 @@
-# import uuid
-# from pathlib import Path
-# from io import BytesIO
+from io import BytesIO
+import uuid
+from pathlib import Path
 
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.storage.blob import BlobClient
 from django.conf import settings
 
-# from . import models
+from . import models
+
+
+ALLOWED_EXTENTIONS = ['.pdf','.doc','.docx']
 
 def create_blob_client(file_name):
 
@@ -17,7 +20,7 @@ def create_blob_client(file_name):
         vault_url=settings.AZURE_VAULT_ACCOUNT, credential=default_credential
     )
 
-    storage_credentials = secret_client.get_secret(name="storage-key")
+    storage_credentials = secret_client.get_secret(name=settings.AZURE_STORAGE_KEY_NAME)
 
     return BlobClient(
         account_url=settings.AZURE_STORAGE_ACCOUNT,
@@ -28,14 +31,35 @@ def create_blob_client(file_name):
 
 
 def check_file_ext(path):
-    pass
+    ext = Path(path).suffix
+    return ext in ALLOWED_EXTENTIONS
+
 
 def download_blob(file):
-    pass
-
+    blob_client = create_blob_client(file)
+    if not blob_client.exists():
+        return
+    blob_content = blob_client.download_blob()
+    return blob_content
+    
 
 def save_file_url_to_db(file_url):
-    pass
+    new_file = models.File.objects.create(file_url=file_url)
+    new_file.save()
+    return new_file
 
 def upload_file_to_blob(file):
-    pass
+
+    if not check_file_ext(file.name):
+        return
+
+    file_prefix = uuid.uuid4().hex
+    ext = Path(file.name).suffix
+    file_name = f"{file_prefix}{ext}"
+    file_content = file.read()
+    file_io = BytesIO(file_content)
+    blob_client = create_blob_client(file_name=file_name)
+    blob_client.upload_blob(data=file_io)
+    file_object = save_file_url_to_db(blob_client.url)
+
+    return file_object
--- website/files/views.py
+++ website/files/views.py
@@ -1,25 +1,57 @@
-# from pathlib import Path
-# import mimetypes
+from hashlib import new
+from pathlib import Path
+import mimetypes
 
 from django.shortcuts import render, redirect
-# from django.contrib import messages
-# from django.http import Http404, HttpResponse
+from django.contrib import messages
+from django.http import Http404, HttpResponse
 
-# from .azure_file_controller import upload_file_to_blob, download_blob
+from .azure_file_controller import ALLOWED_EXTENTIONS, download_blob, upload_file_to_blob
 
+from . import models
 
 
 def index(request):
     return render(request, "files/index.html", {})
 
 def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+        ext = Path(file.name).suffix
+        new_file = upload_file_to_blob(file)
+        if not new_file:
+            messages.warning(request, f"{ext} not allowed only accept {', '.join(ext for ext in ALLOWED_EXTENTIONS)} ")
+            return render(request, "files/upload_file.html", {}) 
+        new_file.file_name = file.name
+        new_file.file_extention = ext
+        new_file.save()
+        messages.success(request, f"{file.name} was successfully uploaded")
+        return render(request, "files/upload_file.html", {}) 
+
     return render(request, "files/upload_file.html", {})
 
 def list_files(request):
-    return render(request, "files/list_files.html", {})
+    files = models.File.objects.filter(deleted=0)
+    context = {"files": files}
+    return render(request, "files/list_files.html", context=context)
 
 def download_file(request, file_id):
-    pass
+    file = models.File.objects.get(pk=file_id)
+    file_name = file.file_name
+    file_type, _ = mimetypes.guess_type(file_name)
+    url = file.file_url
+    blob_name = url.split("/")[-1]
+    blob_content = download_blob(blob_name)
+    if blob_content:
+        response = HttpResponse(blob_content.readall(), content_type=file_type)
+        response['Content-Disposition'] = f'attachment; filename={file_name}'
+        messages.success(request, f"{file_name} was successfully downloaded")
+        return response
+    return Http404
+
 
 def delete_file(request,file_id):
+    file = models.File.objects.get(pk=file_id)
+    file.deleted = 1
+    file.save()
     return redirect("list_files")
